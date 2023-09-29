84dd7da94665faae7d2019e231507242732acbee
balandina-o <balandina.olga.2021@gmail.com>
2023-09-29 20:14:10 +0500

    Добавление вопроса

--- src/component/AddQuestionModal.jsx
+++ src/component/AddQuestionModal.jsx
@@ -1,7 +1,47 @@
-import React from "react";
-import { Modal, Button, Form } from "react-bootstrap";
+import React, { useContext, useState } from "react";
+import { Modal, Form, Button } from "react-bootstrap";
+import { Context } from "../index";
 
-const AddQuestionModal = ({ show, onClose }) => {
+const AddQuestionModal = ({ show, onClose, test_id }) => {
+  const { tests } = useContext(Context);
+  const [question, setQuestion] = useState("");
+  const [file, setFile] = useState(null);
+  const [newAnswers, setNewAnswers] = useState([]);
+  const addNewAnswer = () => {
+    setNewAnswers([...newAnswers, { id: Date.now(), name: "" }]);
+  };
+  const selectFile = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const deleteAnswer = (a_id) => {
+    setNewAnswers(newAnswers.filter((answer) => answer.id !== a_id));
+  };
+  const editAnswer = (name, a_id) => {
+    setNewAnswers(
+      newAnswers.map((answer) => {
+        if (answer.id == a_id) {
+          answer.name = name;
+          return answer;
+        }
+        return answer;
+      })
+    );
+  };
+
+  const saveQuestion = () => {
+    const newQuestion = {
+      id: Date.now(),
+      test_id: test_id,
+      question: question,
+      image:
+        "https://avatars.mds.yandex.net/get-mpic/3614670/img_id6299576373330854786.jpeg/orig",
+      answers: newAnswers,
+    };
+    tests.addQuestion(newQuestion);
+    onClose();
+  };
+  console.log("Варианты ответов: ", newAnswers);
   return (
     <div>
       <Modal
@@ -12,12 +52,54 @@ const AddQuestionModal = ({ show, onClose }) => {
         centered
       >
         <Modal.Header closeButton>
-          <Modal.Title>Добавление нового теста</Modal.Title>
+          <Modal.Title>Создание опроса</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form>
-            <Form.Control type="text" placeholder="Название" className="mt-3" />
-            <Form.Control type="file" className="mt-3" />
+            <Form.Control
+              type="text"
+              placeholder="Формулировка вопроса..."
+              className="mt-3"
+              value={question}
+              onChange={(event) => setQuestion(event.target.value)}
+            />
+            <Form.Group controlId="formFile" className="mb-3">
+              <Form.Label className="mt-3">
+                Выберите картинку для вопроса
+              </Form.Label>
+              <Form.Control
+                type="file"
+                className="mt-2"
+                onChange={selectFile}
+              />
+            </Form.Group>
+            <Form.Label className="mt-3">Варианты ответа:</Form.Label>
+            <div>
+              {newAnswers.map((answer) => (
+                <div key={answer.id} className="d-flex">
+                  {" "}
+                  <Form.Control
+                    type="text"
+                    className="mt-3"
+                    value={answer.name}
+                    onChange={(e) => editAnswer(e.target.value, answer.id)}
+                  />
+                  <Button
+                    variant="danger"
+                    style={{ width: "30px", height: "30px" }}
+                    className=" ms-3 mt-3 p-0"
+                    onClick={() => deleteAnswer(answer.id)}
+                  >
+                    &#10008;
+                  </Button>
+                </div>
+              ))}
+            </div>
+            <div>
+              <Button variant="primary" className="mt-3" onClick={addNewAnswer}>
+                Добавить
+              </Button>
+            </div>
           </Form>
         </Modal.Body>
 
@@ -25,7 +107,9 @@ const AddQuestionModal = ({ show, onClose }) => {
           <Button variant="secondary" onClick={() => onClose()}>
             Закрыть
           </Button>
-          <Button variant="primary">Сохранить</Button>
+          <Button variant="primary" onClick={saveQuestion}>
+            Сохранить
+          </Button>
         </Modal.Footer>
       </Modal>
     </div>
